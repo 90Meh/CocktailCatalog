f67d7cc64c2e22ddc36e4da3d42237b9e978c83e
meh <meh@IT5>
2023-08-15 16:47:19 +0800

    Убран лишний бот. Пытался сохранить List в Json В процессе

--- CocktailCatalog/Telegram/MyTelegramBot.cs
+++ CocktailCatalog/Telegram/MyTelegramBot.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
 using Telegram.Bot;
-using System.Xml;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types.Enums;
 
